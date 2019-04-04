f616f33f1f856a4f779a5b33368bbdd8c01239bc
rguillom <remi.guillomon@polytech-lille.fr>
2019-04-04 12:15:26 +0200

    commit : recherche, ajout_dico ; /!\ seg fault

--- tree.c
+++ tree.c
@@ -16,12 +16,6 @@ typedef struct node{
   struct tableau_pt *fils;
 } Node;
 
-Node * init_tree(){
-  Node *pn=malloc(sizeof(Node));
-  pn->fin=false;
-  pn->fils=NULL;
-  return pn;
-}
 
 /* Libération récursive d'un noeud */
 void free_node(Node **pn){
@@ -39,6 +33,16 @@ void free_node(Node **pn){
   }
   return;
 }
+
+
+/* Fonction initialisant un arbre ; renvoie un pointeur de noeud*/
+void init_tree(Node **pn){
+  *pn=malloc(sizeof(Node));
+  (*pn)->fin=false;
+  (*pn)->fils=NULL;
+  return ;
+}
+
 
 /* Fonction initialisant un tableau de pointeurs de noeuds */
 void init_tableau_pt(tableau_pt **pt){
@@ -81,23 +85,57 @@ int indice(Node *pn, char mot[TAILLE_MOT]){
 }
 
 
-
-
-
-void ajout_dico(Node **dico, char mot[TAILLE_mot]){
-  if (*dico == NULL){
-    *dico = init_tree();
+/* Fonction recherchant si un fils associé à un caractère existe */
+bool recherche(Node *pn, char c){
+  int i = c-'a';
+  //bool condition = (pn->fils->T[i] == NULL);
+  if (pn->fils->T[i] == NULL){
+    return false;
   }
-  
-
+  else return true;
 }
 
+
+
+/* Fonction récursive ajoutant le mot (en MINUSCULE) à la suite de l'arbre*/
+int ajout_dico(Node **noeud, char mot[TAILLE_MOT]){
+  if (TAILLE_MOT-1 < strlen(mot)){                        // si le mot ne contient pas '\0', on revoie 0
+    printf("Le mot n'a pas pu être ajouté.\n");
+    return 0;
+  }
+
+  if (*noeud == NULL){                                   // le caractère à ajouter n'était pas présent dans le dico
+    init_tree(noeud);
+  }
+  
+  if (strlen(mot) == 1){                                 // si le mot ne contient qu'un caractère
+    ajout_node(noeud, mot[0], true);
+    return 1;    
+  }
+  else{
+    int i =mot[0]-'a';
+    if (!recherche(*noeud, mot[0])){                     //si le fils n'existe pas
+      
+      init_tree(&(*noeud)->fils->T[i]);
+    }
+    ajout_dico(&(*noeud)->fils->T[i], &mot[1]); 
+  }
+  return 1;
+}
+
+
+//TODO FONCTION CASSE qui met un mot en minuscules
+
+
+
 int main(){
-  //Node *dico=init_tree();
-  
+  Node *dico;
+  init_tree(&dico);
+  ajout_dico(&dico, "abc");
+  ajout_dico(&dico, "ab");
+  ajout_dico(&dico, "abb");
 
-
-  
+  free_node(&dico);
   return 0;
 }
 
